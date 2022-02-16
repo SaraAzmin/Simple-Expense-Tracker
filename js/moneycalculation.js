0806d1370c9756f8eb5792a2d94dedab43c58e07
SaraAzmin <azmin.sara17@gmail.com>
2022-02-16 20:57:04 +0100

    saving and remaining balance calculated

--- js/moneycalculation.js
+++ js/moneycalculation.js
@@ -24,6 +24,29 @@ function calculateExpenseAndBalance(income, food, rent, clothes) {
     const balanceField = document.getElementById('balance-amount');
     let balance = income - expenses;
     balanceField.innerText = balance;
+    return balance;
 }
 
 //click event for save button
+document.getElementById('save-button').addEventListener('click', function () {
+    const savePercent = getInputValue('save-field');
+    calculateSavingAndBalance(savePercent);
+})
+
+function calculateSavingAndBalance(savePercent) {
+
+    const income = getInputValue('income-field');
+    const food = getInputValue('food-field');
+    const rent = getInputValue('rent-field');
+    const clothes = getInputValue('clothes-field');
+    let saving = (income * savePercent) / 100;
+
+    const savingAmount = document.getElementById('saving-amount');
+    savingAmount.innerText = saving;
+
+    const balance = calculateExpenseAndBalance(income, food, rent, clothes);
+    balanceRemaining = balance - saving;
+    const balanceRemainingField = document.getElementById('balance-remaining');
+    balanceRemainingField.innerText = balanceRemaining;
+
+}
