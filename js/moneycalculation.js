8c6e25c3989dc8af1de8d5bdb4e9e28733590d39
SaraAzmin <azmin.sara17@gmail.com>
2022-02-17 00:26:06 +0100

    error msg added

--- js/moneycalculation.js
+++ js/moneycalculation.js
@@ -5,13 +5,25 @@ document.getElementById('calculate-button').addEventListener('click', function (
     const rent = getInputValue('rent-field');
     const clothes = getInputValue('clothes-field');
 
+    //error-messeges
+    const notNumberError = document.getElementById('not-number-error');
+    const negNumberError = document.getElementById('neg-number-error');
+
     if (income >= 0 && food >= 0 && rent >= 0 && clothes >= 0) {
+        notNumberError.style.display = 'none';
+        negNumberError.style.display = 'none';
         calculateExpenseAndBalance(income, food, rent, clothes);
+
     }
     else if (isNaN(income) || isNaN(food) || isNaN(rent) || isNaN(clothes)) {
-        console.log('not number');
-    }
 
+        notNumberError.style.display = 'block';
+        negNumberError.style.display = 'none';
+    }
+    else if (income < 0 || food < 0 || rent < 0 || clothes < 0) {
+        negNumberError.style.display = 'block';
+        notNumberError.style.display = 'none';
+    }
 })
 
 //function for fatching value from input fields
@@ -24,21 +36,30 @@ function getInputValue(inputId) {
 //function to calculate expenses and balance
 function calculateExpenseAndBalance(income, food, rent, clothes) {
 
+    //error msg
+    const overExpenseError = document.getElementById('over-expense-error');
+
     //expenses calculation
     const expenseField = document.getElementById('expense-amount');
     let expenses = food + rent + clothes;
 
+    //balance calculation
+    const balanceField = document.getElementById('balance-amount');
+    let balance = income - expenses;
+
     if (expenses <= income) {
+        overExpenseError.style.display = 'none';
         expenseField.innerText = expenses;
 
         //balance after expenses
-        const balanceField = document.getElementById('balance-amount');
-        let balance = income - expenses;
         balanceField.innerText = balance;
         return balance;
     }
     else {
-        console.log('cannot spend more than income');
+        //error msg for expense exceeding income
+        overExpenseError.style.display = 'block';
+        expenseField.innerText = '0';
+        balanceField.innerText = '0';
     }
 
 }
@@ -46,14 +67,23 @@ function calculateExpenseAndBalance(income, food, rent, clothes) {
 //click event for save button
 document.getElementById('save-button').addEventListener('click', function () {
     const savePercent = getInputValue('save-field');
+
+    //error-messeges
+    const notNumberError = document.getElementById('save-not-number-error');
+    const negNumberError = document.getElementById('save-neg-number-error');
+
     if (savePercent >= 0) {
         calculateSavingAndBalance(savePercent);
+        notNumberError.style.display = 'none';
+        negNumberError.style.display = 'none';
     }
     else if (isNaN(savePercent)) {
-        console.log('not number');
+        notNumberError.style.display = 'block';
+        negNumberError.style.display = 'none';
     }
     else if (savePercent < 0) {
-        console.log('negative');
+        negNumberError.style.display = 'block';
+        notNumberError.style.display = 'none';
     }
 })
 
@@ -64,24 +94,35 @@ function calculateSavingAndBalance(savePercent) {
     const food = getInputValue('food-field');
     const rent = getInputValue('rent-field');
     const clothes = getInputValue('clothes-field');
+
     const balance = calculateExpenseAndBalance(income, food, rent, clothes);
 
+    //error msg
+    const overSavingError = document.getElementById('over-saving-error');
+
     //saving calculation
-    let saving = (income * savePercent) / 100;
+    let saving = parseInt((income * savePercent) / 100);
     const savingAmount = document.getElementById('saving-amount');
+
+    //balance calculation
+    balanceRemaining = balance - saving;
+    const balanceRemainingField = document.getElementById('balance-remaining');
 
     //if balance is less than saving saving not possible, remaining balance is not calculated
     //otherwise remaining balance will give negative value
-    if (savingAmount <= balance) {
+    if (saving <= balance) {
+
+        overSavingError.style.display = 'none';
         savingAmount.innerText = saving;
 
         //balance after saving
-        balanceRemaining = balance - saving;
-        const balanceRemainingField = document.getElementById('balance-remaining');
         balanceRemainingField.innerText = balanceRemaining;
     }
     else {
-        console.log('not enough money for saving');
+        //error msg for saving exceeding balance
+        overSavingError.style.display = 'block';
+        savingAmount.innerText = '0';
+        balanceRemainingField.innerText = '0';
     }
 
 }
